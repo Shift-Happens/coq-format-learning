84f8efe0bf2255595942817be5394b7b43794f66
Arkadiusz Kubiszewski <90008035+Shift-Happens@users.noreply.github.com>
2024-11-04 18:17:45 +0100

    simple trained model on 500 coq format dataset

--- data/sample_input/mathcomp_style.v
+++ data/sample_input/mathcomp_style.v
@@ -1,19 +1,67 @@
 From mathcomp Require Import all_ssreflect.
 
-Lemma example_proof n m : n + m = m + n.
+(* Lemma: Commutativity of addition *)
+Lemma example_proof (n m : nat) : n + m = m + n.
 Proof.
-move=> n m.
-elim: n => [|n' IHn] /=.
-  by rewrite addnC.
-by rewrite IHn addnS.
+  move=> n m.
+  elim: n => [|n' IHn] /=.
+  - by rewrite addnC.
+  - by rewrite IHn addnS.
 Qed.
 
-Definition is_even n := exists k, n = k + k.
+(* Definition: Evenness *)
+Definition is_even (n : nat) := exists k, n = k + k.
 
-Lemma even_plus : forall n m,
-  is_even n -> is_even m -> is_even (n + m).
+(* Lemma: Evenness is preserved under addition *)
+Lemma even_plus (n m : nat) : is_even n -> is_even m -> is_even (n + m).
 Proof.
-move=> n m [k1 Hk1] [k2 Hk2].
-exists (k1 + k2).
-by rewrite Hk1 Hk2 addnA [k2+_]addnC addnA.
+  move=> n m [k1 Hk1] [k2 Hk2].
+  exists (k1 + k2).
+  by rewrite Hk1 Hk2 addnA [k2+_]addnC addnA.
+Qed.
+
+(* Lemma: Evenness is preserved under multiplication *)
+Lemma even_mult (n m : nat) : is_even n -> is_even m -> is_even (n * m).
+Proof.
+  move=> n m [k1 Hk1] [k2 Hk2].
+  exists (k1 * k2).
+  by rewrite Hk1 Hk2 mulnC mulnA.
+Qed.
+
+(* Lemma: Oddness is preserved under addition *)
+Lemma odd_plus (n m : nat) : ~is_even n -> ~is_even m -> ~is_even (n + m).
+Proof.
+  move=> n m Hn Hm.
+  intros [k Hk].
+  apply: Hn.
+  exists k.
+  by rewrite -Hk addnC addnK.
+Qed.
+
+(* Lemma: Oddness is preserved under multiplication *)
+Lemma odd_mult (n m : nat) : ~is_even n -> ~is_even m -> ~is_even (n * m).
+Proof.
+  move=> n m Hn Hm.
+  intros [k Hk].
+  apply: Hn.
+  exists k.
+  by rewrite -Hk mulnC mulnK.
+Qed.
+
+(* Lemma: The sum of two odd numbers is even *)
+Lemma odd_sum_even (n m : nat) : ~is_even n -> ~is_even m -> is_even (n + m).
+Proof.
+  move=> n m Hn Hm.
+  exists (n / 2 + m / 2).
+  by rewrite addn_div2 addn_div2 addnA.
+Qed.
+
+(* Lemma: If n is odd, then n^2 is odd *)
+Lemma odd_square (n : nat) : ~is_even n -> ~is_even (n * n).
+Proof.
+  move=> n Hn.
+  intros [k Hk].
+  apply: Hn.
+  exists (k * 2).
+  by rewrite -Hk mulnS mulnC.
 Qed.
